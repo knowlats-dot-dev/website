5d53a6c83b2a6842204ab1560125c0d460f035dc
Kotchakorn Wongwisutigul <icekotcha@gmail.com>
2023-12-09 19:47:09 +0700

    fix: :bug: Fix date displaying (wrong timezone)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,27 +1,22 @@
 import type { CollectionEntry } from 'astro:content'
+import dayjs from 'dayjs'
 import path from 'path'
+import { TIMEZONE } from '$/config'
 
-const MONTHS = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec'
-]
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+export const getLocalDate = (date: Date) => {
+  return dayjs(date).tz(TIMEZONE)
+}
 
 export const toTitleCase = (str: string) =>
   str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
   })
-
-export const getMonthName = (date: string) => MONTHS[new Date(date).getMonth()]
 
 export const getSlugFromPathname = (pathname: string) =>
   path.basename(pathname, path.extname(pathname))
